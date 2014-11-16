48ac7850c10825d403b358084d2f40eda0957597
Jason Brillon <Jason62513@gmail.com>
2014-11-16 14:26:33 -0500

    altered work

---
+++ js/colorscroll.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+
+   var hT = $('#JBrillz').offset().top,
+       hH = $('#JBrillz').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH)){
+       $(".nav-one").css("color","#7D1935");   	
+       $(".nav-two").css("color","#FDF3E7");   	
+} else {
+       $(".nav-one").css("color","#FDF3E7");
+   }
+
+   var hT = $('#Portfolio').offset().top,
+       hH = $('#Portfolio').height(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH)){
+       $(".nav-one").css("color","#FDF3E7");   	
+       $(".nav-two").css("color","#7D1935");
+   } else {
+       $(".nav-two").css("color","#FDF3E7");
+   }
+});
+
+$('.box1').click(function() {
+   window.location = "https://github.com/jbrill01/jcc";
+});
+});
